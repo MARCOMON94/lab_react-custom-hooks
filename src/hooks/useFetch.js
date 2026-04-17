361119c2d5fbe4003b6f2e50334d01456a6f0b92
MARCO <125563435+MARCOMON94@users.noreply.github.com>
2026-04-17 09:16:22 +0100

    test

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,17 +2,20 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function useFetch(url) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const response = await axios.get(url);
         setData(response.data);
+        setError(null);
       } catch (err) {
         setError(err);
+        setData(null);
       } finally {
         setLoading(false);
       }
@@ -22,6 +25,7 @@ function useFetch(url) {
   }, [url]);
 
   return { data, loading, error };
+  
 }
 
 export default useFetch;
